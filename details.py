5bf3099d48786def31ec470b2568916818f35040
BrhamaKumar <146445615+BrhamaKumar@users.noreply.github.com>
2024-02-08 14:51:09 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
 api_id = " 20810825"
 api_hash = "707e67f53b4593a3e9b6b424311f84d0"
-bot_token = "6770894410:AAHEePgcZsnamzxtXyOzm6tXWrOLmbHXaRI" 
+bot_token = "6945899533:AAHqvDra4_MtjqwoG1SufiU0PfQFRVj9TOE" 
 sudo_group = -1002033924003
